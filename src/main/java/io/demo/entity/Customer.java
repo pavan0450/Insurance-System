b343e89c4403b0c6c69a1a817abdb58fc9d1f9be
SiddharthaNamani <siddhunamani@gmail.com>
2017-03-30 16:24:05 -0400

    customer class updated with hibernate annotations

--- src/main/java/io/demo/entity/Customer.java
+++ src/main/java/io/demo/entity/Customer.java
@@ -1,11 +1,28 @@
 package io.demo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Entity
+@Table
 public class Customer {
 
+	@Id
+	@GenericGenerator(name="customUUID", strategy="uuid2")
+	@GeneratedValue(generator="customUUID")
 	private String login_id;
+	
 	private String name;
 	private String date_of_birth;
 	private int phone;
+	
+	@Column(unique=true)
 	private String email;
 	private String password;
 	private String gender;
